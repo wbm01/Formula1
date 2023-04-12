b46c73577ba1300504794cba2f9a7fdeb94ee7f0
Willian Marioti <willian.marioti@gmail.com>
2023-04-12 18:02:20 -0300

    Add project files.

---
+++ Formula1Mongo/Driver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Formula1Mongo
+{
+    //[BsonIgnoreExtraElements] ignora elementos extras vindo do Mongo que não estão
+    //na classe abaixo
+    internal class Driver
+    {
+        [BsonId] //referencia o BsonId
+        [BsonRepresentation(BsonType.ObjectId)] //transforma em um ObjectId
+        public string ? Id { get; set; }
+
+        [BsonElement("Driver")]
+        public string ? Name { get; set; }
+
+        [BsonElement("Abbreviation")]
+        public string ? Abbreviation { get; set;}
+
+        [BsonElement("No")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int Number { get; set; }
+
+        [BsonElement("Team")]
+        public string ? Team { get; set; }
+
+        [BsonElement("Country")]
+        public string ? Country { get; set; }
+
+        [BsonElement("Date of Birth")]
+        public string ? BirthDate { get; set; }
+
+        public Driver(string? name, string? abbreviation, int number, string? 
+            team, string? country, string? birthDate)
+        {
+            Name = name;
+            Abbreviation = abbreviation;
+            Number = number;
+            Team = team;
+            Country = country;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "\nNome: " + (Name) + "\nAbreviação: " + (Abbreviation) + "\nNúmero do Carro: " + (Number) + "\nEquipe: " + (Team) + "\nPaís de Origem: " +
+                (Country) + "\nData de Nascimento: " + (BirthDate);
+        }
+    }
+}
---
+++ Formula1Mongo/Program.cs
@@ -0,0 +1,113 @@
+﻿using Formula1Mongo;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MongoClient mongo = new MongoClient("mongodb://localhost:27017");
+
+        /*var listabancos = mongo.ListDatabaseNames().ToList();
+
+        foreach(var banco in listabancos)
+        {
+            Console.WriteLine(banco);
+        }*/
+
+        var basededados = mongo.GetDatabase("Formula1");
+        var collection = basededados.GetCollection<BsonDocument>("Pilotos");
+        var document = collection.Find(new  BsonDocument()).ToList();
+
+        //SELECT
+        /*Console.Write("\nInforme o nome do piloto: ");
+        var nome = Console.ReadLine();
+
+        var filtro = Builders<BsonDocument>.Filter.Regex("Driver", nome);
+
+        var piloto = collection.Find(filtro).FirstOrDefault();
+
+        var p = BsonSerializer.Deserialize<Driver>(piloto);
+
+        Console.WriteLine(p.ToString());*/
+
+        /*foreach(var documents in document)
+        {
+            Console.WriteLine(documents.ToString());
+            Console.ReadLine();
+        }*/
+
+
+        /*var filtro = Builders<BsonDocument>.Filter.Regex("Driver", nome);
+
+        var piloto = collection.Find(filtro).FirstOrDefault();
+
+        Console.WriteLine(piloto);
+
+        Console.Write("\nInforme o nome da equipe: ");
+        var equipe = Console.ReadLine();
+
+        filtro = Builders<BsonDocument>.Filter.Regex("Team", equipe);
+
+        var pilotos = collection.Find(filtro).ToList();
+
+        foreach(var driver in pilotos)
+        {
+            Console.WriteLine(driver);
+        }*/
+
+        //INSERT
+        /*Console.Write("\nNome: ");
+        var n = Console.ReadLine();
+
+        Console.Write("\nAbreviação: ");
+        var a = Console.ReadLine();
+
+        Console.Write("\nNúmero: ");
+        var nu = int.Parse(Console.ReadLine());
+
+        Console.Write("\nEquipe: ");
+        var e = Console.ReadLine();
+
+        Console.Write("\nPaís: ");
+        var p = Console.ReadLine();
+
+        Console.Write("\nData de Nascimento: ");
+        var d = Console.ReadLine();
+
+        Driver driver = new Driver(n,a,nu,e,p,d);
+
+        Console.WriteLine(driver.ToString());
+
+        var dr = new BsonDocument
+        {
+            {"Driver", driver.Name},
+            {"Abbreviation", driver.Abbreviation},
+            {"No", driver.Number},
+            {"Team", driver.Team},
+            {"Country", driver.Country},
+            {"Date of Birth", driver.BirthDate}
+        };
+
+        Console.WriteLine(dr);
+
+        collection.InsertOne(dr);*/
+
+        //UPDATE
+        Console.Write("\nInforme o nome do piloto: ");
+        string n = Console.ReadLine();
+
+        var p = collection.Find(Builders<BsonDocument>.Filter.Regex("Driver", n)).First();
+
+        var driver = BsonSerializer.Deserialize<Driver>(p);
+
+        //Console.Write("\nInforme o novo número: ");
+        //int numero = int.Parse(Console.ReadLine());
+
+
+        //collection.UpdateOne(Builders<BsonDocument>.Filter.Regex("Driver", n), Builders<BsonDocument>.Update.Set("No", numero));
+
+        //DELETE
+        collection.FindOneAndDelete(p);
+    }
+}
